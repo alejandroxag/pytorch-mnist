b9d0b89a704346ae3728bea05fa34f1466f1cc24
alejandroxag <alejandro.alvarez.1804@gmail.com>
2020-12-30 22:31:03 -0500

    CNN to recognize digits from the MNIST dataset. FINISHED.

--- setup.ps1
+++ setup.ps1
@@ -1,8 +1,10 @@
 # To run: PS <filepath>> .\setup.ps1 (enter)
-conda env remove -n pytorch_mnist_dec3020
-conda create -n pytorch_mnist_dec3020 python=3.8.3
+remove-item -path C:\Users\aleja\anaconda3\envs\pytorch_mnist_dec3020 # Change path. This is necessary only when the environment was already created because f*** conda remove does not work.
+conda deactivate -n pytorch_mnist_dec3020
+conda env remove -n pytorch_mnist_dec3020 -y
+conda create -n pytorch_mnist_dec3020 python=3.8.3 -y
 conda activate pytorch_mnist_dec3020
-conda install jupyter
-conda install matlplotlib
-conda install pytorch
+conda install jupyter -y
+conda install matplotlib -y
+conda install pytorch torchvision torchaudio cudatoolkit=10.2 -c pytorch -y
 python -m ipykernel install --user --name pytorch_mnist_dec3020 # <- To tell the machine to use this environment for the notebooks
